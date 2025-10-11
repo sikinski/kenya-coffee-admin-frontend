b8bb8e49dce62700f494c9a5172bedf4b755400d
sikinski <ulyana.sikinski@gmail.com>
2025-10-11 15:16:13 +0500

    added websocket

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,8 +8,8 @@ export default defineNuxtConfig({
 
     runtimeConfig: {
         public: {
-            backend_address: process.env.BACKEND_ADDRESS,
-            ws_address: process.env.WS_ASSRESS
+            backend_address: process.env.WS_ADDRESS,
+            ws_address: process.env.WS_ADDRESS
         }
     },
 
