db9bf662c3a5f106bea8beb6820976fce91a7e0d
sikinski <ulyana.sikinski@gmail.com>
2025-10-13 14:40:22 +0500

    trying to fix

--- utils/getShortDateTime.js
+++ utils/getShortDateTime.js
@@ -1,20 +1,29 @@
-import moment from 'moment-timezone'
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc.js'
+import timezone from 'dayjs/plugin/timezone.js'
+import localizedFormat from 'dayjs/plugin/localizedFormat.js'
+import 'dayjs/locale/ru.js'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.extend(localizedFormat)
+dayjs.locale('ru')
+
+const TZ = 'Asia/Yekaterinburg'
 
 export function getShortDateTime(isoDate) {
-    const tz = 'Asia/Yekaterinburg'
-    const date = moment.tz(isoDate, tz)
-    const now = moment.tz(tz)
+    const date = dayjs(isoDate).tz(TZ)
+    const now = dayjs().tz(TZ)
 
     if (date.isSame(now, 'day')) {
-        // Сегодня: только время
+        // Сегодня
         return date.format('HH:mm')
-    } else if (date.isSame(now.clone().subtract(1, 'day'), 'day')) {
+    } else if (date.isSame(now.subtract(1, 'day'), 'day')) {
         // Вчера
         return `Вчера, ${date.format('HH:mm')}`
     } else {
-        // Любой другой день
-        // Формат: 10 сен 14:35 или с годом, если не текущий
-        const format = date.year() === now.year() ? 'D MMM HH:mm' : 'D MMM YYYY HH:mm'
-        return date.format(format)
+        // Другие дни
+        const formatStr = date.year() === now.year() ? 'D MMM HH:mm' : 'D MMM YYYY HH:mm'
+        return date.format(formatStr)
     }
 }
